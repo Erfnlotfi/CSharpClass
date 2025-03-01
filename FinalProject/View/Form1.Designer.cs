b5114845559aabcdc528faab6a807a31ced9f5d2
Student <efi137986@gmail.com>
2025-03-01 20:26:58 +0330

    initial

---
+++ FinalProject/View/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace view
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnPerson = new Button();
+            btnProduct = new Button();
+            SuspendLayout();
+            // 
+            // btnPerson
+            // 
+            btnPerson.Location = new Point(362, 153);
+            btnPerson.Name = "btnPerson";
+            btnPerson.Size = new Size(182, 86);
+            btnPerson.TabIndex = 0;
+            btnPerson.Text = "Person";
+            btnPerson.UseVisualStyleBackColor = true;
+            // 
+            // btnProduct
+            // 
+            btnProduct.Location = new Point(705, 153);
+            btnProduct.Name = "btnProduct";
+            btnProduct.Size = new Size(182, 86);
+            btnProduct.TabIndex = 1;
+            btnProduct.Text = "Product";
+            btnProduct.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1259, 450);
+            Controls.Add(btnProduct);
+            Controls.Add(btnPerson);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnPerson;
+        private Button btnProduct;
+    }
+}
---
+++ FinalProject/View/Form2.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace view
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAdd = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            btnRefresh = new Button();
+            btnBack = new Button();
+            label1 = new Label();
+            txtFirstName = new TextBox();
+            SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(87, 19);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(90, 37);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(274, 19);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(90, 37);
+            btnEdit.TabIndex = 1;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(470, 19);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(90, 37);
+            btnDelete.TabIndex = 2;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.Location = new Point(658, 19);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(90, 37);
+            btnRefresh.TabIndex = 3;
+            btnRefresh.Text = "Refresh";
+            btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            btnBack.Location = new Point(851, 19);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(90, 37);
+            btnBack.TabIndex = 4;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(87, 126);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 15);
+            label1.TabIndex = 5;
+            label1.Text = "First Name :";
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Location = new Point(176, 123);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(384, 23);
+            txtFirstName.TabIndex = 6;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1044, 450);
+            Controls.Add(txtFirstName);
+            Controls.Add(label1);
+            Controls.Add(btnBack);
+            Controls.Add(btnRefresh);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnAdd);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAdd;
+        private Button btnEdit;
+        private Button btnDelete;
+        private Button btnRefresh;
+        private Button btnBack;
+        private Label label1;
+        private TextBox txtFirstName;
+    }
+}
