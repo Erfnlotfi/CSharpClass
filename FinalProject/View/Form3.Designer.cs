d074a7da3299e8d6ff8c3af0f18b3a281b2a8db5
ErfanLotfi <Student@307-ST9>
2025-03-08 21:03:14 +0330

    view form 3

---
+++ FinalProject/View/Form3.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace view
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtFirstName = new TextBox();
+            btnBack = new Button();
+            btnRefresh = new Button();
+            btnDelete = new Button();
+            btnEdit = new Button();
+            btnAdd = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Location = new Point(227, 130);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(384, 23);
+            txtFirstName.TabIndex = 12;
+            // 
+            // btnBack
+            // 
+            btnBack.Location = new Point(902, 26);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(90, 37);
+            btnBack.TabIndex = 11;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = true;
+            btnBack.Click += btnBack_Click;
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.Location = new Point(709, 26);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(90, 37);
+            btnRefresh.TabIndex = 10;
+            btnRefresh.Text = "Refresh";
+            btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(521, 26);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(90, 37);
+            btnDelete.TabIndex = 9;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(325, 26);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(90, 37);
+            btnEdit.TabIndex = 8;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(138, 26);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(90, 37);
+            btnAdd.TabIndex = 7;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(138, 133);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 15);
+            label1.TabIndex = 13;
+            label1.Text = "First Name :";
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1261, 450);
+            Controls.Add(label1);
+            Controls.Add(txtFirstName);
+            Controls.Add(btnBack);
+            Controls.Add(btnRefresh);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnAdd);
+            Name = "Form3";
+            Text = "Form3";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtFirstName;
+        private Button btnBack;
+        private Button btnRefresh;
+        private Button btnDelete;
+        private Button btnEdit;
+        private Button btnAdd;
+        private Label label1;
+    }
+}
