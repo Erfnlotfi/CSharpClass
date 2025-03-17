8d2cea88f72ef7446fe375f2c855ccf770d1a654
Student <efi137986@gmail.com>
2025-03-17 17:42:54 +0330

    Person and products clases are applied

---
+++ FinalProject/Model/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Model
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int price { get; set; }
+    }
+}
