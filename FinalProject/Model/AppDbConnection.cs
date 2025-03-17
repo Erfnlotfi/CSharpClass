94bfcd9431708a07583ac3ade68f65567345bca3
Student <efi137986@gmail.com>
2025-03-17 20:20:30 +0330

    Half of DB Done

---
+++ FinalProject/Model/AppDbConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model
+{
+    public class AppDbConnection : DbContext
+    {
+        public AppDbConnection(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
+}
