fa45ecd6031a9dfe6d386e916f0d474ad67e51a4
Erfnlotfi <efi137986@gmail.com>
2025-04-10 14:17:24 +0330

    GetPerson Dto compelet

--- FinalProject/ApplicationService/DTOs/GetPersonDTO.cs
+++ FinalProject/ApplicationService/DTOs/GetPersonDTO.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model.DomainModel
+namespace ApplicationService.DTOs
 {
-    public class Person
+    class GetPersonDTO
     {
-        [Key]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int NationalId { get; set; }
